7c197493e0a6fd13e2922f61458e3341f5091224
Andreas Sander <andreas.sander@hotmail.de>
2016-04-20 22:36:15 +0200

    AngularJS 2.0 - Add Initial Configuration
    Add initial AngularJS 2.0 configuration resulting in a 'Hello World'.

---
+++ app/cantina/main.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'cantina',
+    template: '<h1>Hello World!</h1>'
+})
+
+export class CantinaMain {
+
+}
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {CantinaMain} from 'app/cantina/main';
+
+bootstrap(CantinaMain);
